443e5bcd1757e940164eb47ffce2afd6bb090526
Daniel Garofalo <danielgarofalo@daniels-mbp-2.home>
2019-03-06 08:34:07 -0600

    Colection view/spinner and label customization

--- PixelCity/Controller/MapViewController.swift
+++ PixelCity/Controller/MapViewController.swift
@@ -27,7 +27,8 @@ class MapViewController: UIViewController, UIGestureRecognizerDelegate {
     var spinner : UIActivityIndicatorView?
     var progressLabel : UILabel?
     var screenSize = UIScreen.main.bounds
-    
+    var collectionView : UICollectionView?
+    var flowLayout = UICollectionViewLayout() //To create a collection view programatoccaly, you need to add a layout
     
     
     
@@ -50,6 +51,18 @@ class MapViewController: UIViewController, UIGestureRecognizerDelegate {
         configureLocationServices()
         //Double tap capability
         addDoubleTap()
+        //Instance of collectionView
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: flowLayout)
+        collectionView?.register(PhotoCell.self, forCellWithReuseIdentifier: "reusableCell")
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        collectionView?.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        
+        
+        
+        pullUpImageView.addSubview(collectionView!)
+        
+        
         
     }
     
@@ -107,22 +120,42 @@ class MapViewController: UIViewController, UIGestureRecognizerDelegate {
         }
     }
     
-    //Spinner to indicate activity
+    //Adding spinner to indicate activity
     func addSpinner(){
         //Customization of the spinner and adding it specifically to the pullUpView
         spinner = UIActivityIndicatorView()
-        spinner?.center = CGPoint(x: (screenSize.width / 2) - ((spinner?.frame.width)! / 2), y: 150)
+        spinner?.center = CGPoint(x: (screenSize.width / 2) - ((spinner?.frame.width)! / 2), y: 130)
         spinner?.style = .whiteLarge
         spinner?.color = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
         spinner?.startAnimating()
-        pullUpImageView.addSubview(spinner!)
+        collectionView?.addSubview(spinner!)
     }
     
+    //Removing spinner
+    func removeSpinner(){
+        //If there is an spinner already in place, remove it
+        if spinner != nil {
+            spinner?.removeFromSuperview()
+        }
+    }
     
+    //Add progress label
+    func addProgressLabel(){
+        progressLabel = UILabel()
+        progressLabel?.frame = CGRect(x: (screenSize.width / 2) - 100, y: 150, width: 200, height: 20)
+        progressLabel?.font = UIFont(name: "Avenir Next", size: 18)
+        progressLabel?.textColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+        progressLabel?.textAlignment = .center
+        progressLabel?.text = "Loading xx/xx photos"
+        collectionView?.addSubview(progressLabel!)
+    }
     
-    
-    
-    
+    //Remove status label
+    func removeProgressLabel(){
+        if progressLabel != nil {
+            progressLabel?.removeFromSuperview()
+        }
+    }
     
 
     
@@ -151,26 +184,21 @@ class MapViewController: UIViewController, UIGestureRecognizerDelegate {
 extension MapViewController: MKMapViewDelegate {
     
     
-    //Pin customizton
+    //Pin customizaton
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
         //this customization will apply to any location but the one from the user. Therefore, the original location will stay on blue
         if annotation is MKUserLocation {
             return nil
         }
-        
+        //Creation and customization of the pin
         let pinAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "droppablePin")
         pinAnnotation.pinTintColor = #colorLiteral(red: 0.9960784314, green: 0.6509803922, blue: 0.137254902, alpha: 1)
         pinAnnotation.animatesDrop = true
-        
+        //Pin needs to be returned, according to func specification
         return pinAnnotation
-        
     }
-    
-    
-    
-    
-    
+  
+    //Center the map on the location specified
     func centerMapOnUserLocation(){
         //Grabbing coordinates from the user, setting up a radius from that specific coordinates and setting the focus of the map on that specific radius
         guard let coordinate = locationManager.location?.coordinate else { return }
@@ -181,14 +209,20 @@ extension MapViewController: MKMapViewDelegate {
     //base function for doubletap. Requires for the tap on the map to be passed in so the pin will be dropped on the location specified on the map
     @objc func dropPin(sender: UITapGestureRecognizer){
         
-        //Pins need to be removed so we dont have duplicates
+        //Pins need to be removed so we dont have duplicates, every time a pin is dropped
         removePin()
-        //Animate the image view up when dripping the pin
+        //Remove any previous spinner everytime a new pin is dropped
+        removeSpinner()
+        //Remove any previous progresslabels everytime a new pin is dropped
+        removeProgressLabel()
+        //Animate the image view up when dropping the pin
         animateViewUp()
-        //Showig spinner on the pullUpView everytime we drop a pin
+        //Showing spinner on the pullUpView everytime we drop a pin
         addSpinner()
         //Animate the image view down when swiping down
         addSwipe()
+        //Add progress label
+        addProgressLabel()
         
         //touch point
         let touchPoint = sender.location(in: mapView)
@@ -220,7 +254,7 @@ extension MapViewController: MKMapViewDelegate {
 //Extension for the delegation of Location Management
 extension MapViewController : CLLocationManagerDelegate {
     
-    //Checking for status of auth to check on location
+    //Checking for status of auth to check on location. This needs to be requested and reverified
     func configureLocationServices() {
         if authStatus == .notDetermined {
             locationManager.requestAlwaysAuthorization()
@@ -235,8 +269,29 @@ extension MapViewController : CLLocationManagerDelegate {
     }
     
     
-    
-    
-    
-    
 } //EOC
+
+
+
+extension MapViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    //Number of sections
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    //Number of items
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    //Content of items
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as? PhotoCell
+        
+        
+        return cell!
+    }
+    
+    
+}
---
+++ PixelCity/View/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  PixelCity
+//
+//  Created by Daniel Garofalo on 3/6/19.
+//  Copyright © 2019 Daniel Garofalo. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    //Cell init needs to be overridden
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
